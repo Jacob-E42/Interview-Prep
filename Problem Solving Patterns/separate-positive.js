1d555eba65f44002734c9fa8b919e931ab244aa4
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-19 14:52:42 -0500

    made separatePositive() with moving pointers

--- Problem Solving Patterns/separate-positive.js
+++ Problem Solving Patterns/separate-positive.js
@@ -1,4 +1,18 @@
-// add whatever parameters you deem necessary
-function separatePositive() {}
+function separatePositive(arr) {
+	if (arr.length <= 1) return arr;
+	let positiveIdx = 0;
+	let negativeIdx = arr.length - 1;
+	while (positiveIdx < negativeIdx) {
+		let firstVal = arr[positiveIdx];
+		let lastVal = arr[negativeIdx];
+		if (firstVal < 0 && lastVal > 0) {
+			[arr[positiveIdx], arr[negativeIdx]] = [arr[negativeIdx], arr[positiveIdx]];
+			positiveIdx++;
+			negativeIdx--;
+		} else if (firstVal < 0) negativeIdx--;
+		else positiveIdx++;
+	}
+	return arr;
+}
 
 module.exports = separatePositive;
