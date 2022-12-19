691263d4b8a2751734b5d9c9a9ef2c716866068d
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-19 13:37:30 -0500

    implemented sameFrequency using a freq counter

--- Problem Solving Patterns/same-frequency.js
+++ Problem Solving Patterns/same-frequency.js
@@ -1,4 +1,22 @@
+const { freqCount } = require("./valid-anagram");
+
 // add whatever parameters you deem necessary
-function sameFrequency() {}
+function sameFrequency(num1, num2) {
+	const str1 = num1.toString();
+	const str2 = num2.toString();
+	if (str1.length !== str2.length) return false;
+
+	//make freq counts for every digit in both integers
+	const num1Freq = freqCount(str1);
+	const num2Freq = freqCount(str2);
+
+	//loop over one freq count and return false if a given digit is not
+	// represented in the other count, or is represented a different amount of times.
+	for (let digit in num1Freq) {
+		// if (!num2Freq[digit]) return false;
+		if (num1Freq[digit] !== num2Freq[digit]) return false;
+	}
+	return true;
+}
 
 module.exports = sameFrequency;
--- Problem Solving Patterns/two-array-object.js
+++ Problem Solving Patterns/two-array-object.js
@@ -1,6 +1,13 @@
+//twoArrayObject takes two arrays of arbitraty length and returns an object, using
+//the values in the first array as keys, and those in the second as values.
 function twoArrayObject(keys, values) {
+	//to keep track of current index in values array
 	let valueIndex = 0;
 	const obj = {};
+
+	//for every key in the keys array, either set the value for that key as the
+	// corresponding value in the values array, if the values array's length is
+	// equal to or greater than the keys array's. Or if not, set the key to null.
 	for (let key of keys) {
 		if (values[valueIndex]) {
 			obj[key] = values[valueIndex];
