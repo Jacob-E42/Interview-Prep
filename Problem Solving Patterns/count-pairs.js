c0e83d4a4f6f43081f696d168cfbf0e83c4d8e36
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-19 14:27:48 -0500

    implemented countPairs() using a Set

--- Problem Solving Patterns/count-pairs.js
+++ Problem Solving Patterns/count-pairs.js
@@ -1,4 +1,12 @@
 // add whatever parameters you deem necessary
-function countPairs() {}
+function countPairs(arr, target) {
+	let count = 0;
+	const alreadySeen = new Set();
+	for (let num of arr) {
+		if (alreadySeen.has(target - num)) count++;
+		alreadySeen.add(num);
+	}
+	return count;
+}
 
 module.exports = countPairs;
