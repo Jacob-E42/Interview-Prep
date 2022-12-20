1414587d3cb0ae68be81f101afadf8ebdc0d200d
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-20 11:56:30 -0500

    Implemented createAccount() using closures

--- JS Tricky Parts/create-account.js
+++ JS Tricky Parts/create-account.js
@@ -1,5 +1,27 @@
-function createAccount(pin, amount) {
-
+"use strict";
+function createAccount(accountPin, balance = 0) {
+	return {
+		checkBalance: function (pin) {
+			if (accountPin === pin) return `$${balance}`;
+			return "Invalid PIN.";
+		},
+		deposit: function (pin, amount) {
+			if (pin !== accountPin) return "Invalid PIN.";
+			balance += amount;
+			return `Succesfully deposited $${amount}. Current balance: $${balance}.`;
+		},
+		withdraw: function (pin, amount) {
+			if (pin !== accountPin) return "Invalid PIN.";
+			if (amount > balance) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+			balance -= amount;
+			return `Succesfully withdrew $${amount}. Current balance: $${balance}.`;
+		},
+		changePin: function (old, newPin) {
+			if (old !== accountPin) return "Invalid PIN.";
+			accountPin = newPin;
+			return "PIN successfully changed!";
+		}
+	};
 }
 
 module.exports = { createAccount };
