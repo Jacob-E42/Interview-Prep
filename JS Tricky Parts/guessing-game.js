3e820e5d646a1349f3771021c65ca792452c47b9
Jacob Eiferman <jacobeiferman@gmail.com>
2022-12-20 11:23:52 -0500

    implemented guessingGame() with closure

--- JS Tricky Parts/guessing-game.js
+++ JS Tricky Parts/guessing-game.js
@@ -1,5 +1,20 @@
 function guessingGame() {
-
+	const secretNumber = Math.floor(Math.random() * 100);
+	let count = 0;
+	let isOver = false;
+	const func = function (num) {
+		while (!isOver) {
+			count++;
+			if (num === secretNumber) {
+				isOver = true;
+				return `You win! You found ${secretNumber} in ${count} guesses.`;
+			}
+			if (num < secretNumber) return `${num} is too low!`;
+			else return `${num} is too high!`;
+		}
+		return "The game is over, you already won!";
+	};
+	return func;
 }
 
 module.exports = { guessingGame };
